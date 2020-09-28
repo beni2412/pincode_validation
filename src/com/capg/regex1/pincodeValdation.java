5f328bcecc6505ebb67e08d379e1d9fa80c71e44
beni2412 <hardaman97@gmail.com>
2020-09-28 18:48:04 +0530

    [Hardaman] Added: Welcome Message

---
+++ src/com/capg/regex1/pincodeValdation.java
@@ -0,0 +1,10 @@
+package com.capg.regex1;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class pincodeValdation {
+	public static void main(String[] args) {
+	System.out.println("Welcome to pincode validation");
+	}
+
+}
