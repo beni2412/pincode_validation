3c7c500d07b3842a4272fd6fb6c5d43114b6cebe
beni2412 <hardaman97@gmail.com>
2020-09-28 18:58:39 +0530

    [Hardaman] Added: Created regex pattern to validate pin 400088

--- src/com/capg/regex1/pincodeValdation.java
+++ src/com/capg/regex1/pincodeValdation.java
@@ -1,10 +1,22 @@
 package com.capg.regex1;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class pincodeValdation {
 	public static void main(String[] args) {
-	System.out.println("Welcome to pincode validation");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a pincode: ");
+		String pincode = sc.nextLine();
+		Pattern pattern =Pattern.compile("[0-9]{6}$");
+		Matcher matcher = pattern.matcher(pincode);
+		boolean matchfound = matcher.find();
+		if(matchfound) {
+			System.out.println("Match found");
+		}
+		else {
+			System.out.println("Match not found");
+		}
 	}
 
 }
